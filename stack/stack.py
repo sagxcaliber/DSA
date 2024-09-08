ddab6e7786923b57336eaf15aa4f280eec38ea9d
PybySa <sagar@mysticmatrix.in>
2024-09-08 21:16:00 +0530

    stack quick fixes

--- stack/stack.py
+++ stack/stack.py
@@ -1,30 +1,42 @@
-
 class NewNode:
     def __init__(self, value):
         self.value = value
         self.next = None
+
 
 class Stack:
     def __init__(self, value):
         new_node = NewNode(value)
         self.top = new_node
         self.bottom = new_node
+        self.height = 1
 
     def push(self, value):
         new_node = NewNode(value)
         temp_node = self.top
         self.top = new_node
         self.top.next = temp_node
+        self.height += 1
+        return True
 
     def pop(self):
-        temp_node = self.top.next
-        self.top = temp_node
+        if self.height == 0:
+            self.top = None
+            self.bottom = None
+            self.height = 0
+            return None
+        temp_node = self.top
+        self.top = self.top.next
+        self.height -= 1
+        print('pop from stack : ', temp_node.value)
+        return temp_node
 
     def printStack(self):
         temp_node = self.top
         while temp_node is not None:
             print(temp_node.value)
             temp_node = temp_node.next
+
 
 s = Stack(1)
 s.push(2)
@@ -34,5 +46,5 @@ s.push(5)
 s.push(6)
 s.push(7)
 s.pop()
+s.pop()
 s.printStack()
-
